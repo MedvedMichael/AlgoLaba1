a2a3e5b9d6ecaf04fee535468c742c1db8729fd4
MPH <misha.medvedev2001@gmail.com>
2020-02-25 22:46:00 +0200

    Algorithm is done

--- src/app.js
+++ src/app.js
@@ -1,12 +1,27 @@
 const readline = require('readline')
 const shell_sort = require('./shell-sort')
+const chalk = require('chalk')
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-  });
+});
 
-  rl.question("Input length: ",(answer)=>{
-      shell_sort(answer)
-      rl.close();
-  })
+rl.question("Input length: ", (length) => {
+    array = Array.from({ length }, () => Math.round(Math.random() * (length / 2)))
+
+    sortedArray = shell_sort(array)
+    printArray(array)
+    printArray(sortedArray)
+
+    rl.close();
+})
+
+const printArray = (array) => {
+    let output = '[ '
+    array.forEach((element, index) => {
+        output += chalk.greenBright.italic(element) + ((index == array.length - 1) ? ' ' : ', ')
+    })
+    output += ']'
+    console.log(output)
+}
--- src/shell-sort.js
+++ src/shell-sort.js
@@ -1,18 +1,38 @@
-const shell_sort = (length)=>{
-    console.log(getRowOnNumbersByTokuda(length))
-}
+const shell_sort = (inputArray) => {
+    let array = inputArray.slice()
 
-const getRowOnNumbersByTokuda = (length)=>{
-    result = []
-    iter=1
+    numbers = getRowOnNumbersByTokuda(array.length)
+    numbers.forEach(element => {
+        sortArrayOnDeltaNumbers(array, element)
+    });
+
+    return array;
+}
+const getRowOnNumbersByTokuda = (length) => {
+    resultRow = []
+    iter = 1
     data = 1
-    while (data<length){
-        result.push(data)
+    while (data < length) {
+        resultRow.push(data)
         iter++
         data = Math.ceil(1.0 / 5 * (9 * Math.pow(9.0 / 4, iter - 1) - 4))
-    } 
+    }
+    return resultRow;
+}
 
-    return result;
+const sortArrayOnDeltaNumbers = (array, number) => {
+    for (let i = 0; i < number; i++) {
+        for (let j = i + number; j < array.length; j += number) {
+            const key = array[j]
+            let k = j - number
+
+            while (k >= i && array[k] > key) {
+                array[k + number] = array[k]
+                k -= number
+            }
+            array[k + number] = key
+        }
+    }
 }
 
 module.exports = shell_sort
